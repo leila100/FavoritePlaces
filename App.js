da232a92c6d697607dffaecf696718586224643c
Leila <nb.leila10@gmail.com>
2019-06-01 22:48:51 -0500

    Add place with add button and show list of places under input

--- App.js
+++ App.js
@@ -3,14 +3,25 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default class App extends Component {
   state = {
-    placeName: ""
+    placeName: "",
+    places: []
   };
 
   placeNameChangedHandler = val => {
     this.setState({ placeName: val });
   };
 
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") return;
+    this.setState(prevState => {
+      return {
+        places: [...prevState.places, prevState.placeName]
+      };
+    });
+  };
+
   render() {
+    const placesOutput = this.state.places.map((place, i) => <Text key={i}>{place}</Text>);
     return (
       <View style={styles.container}>
         <View style={styles.inputContainer}>
@@ -20,8 +31,9 @@ export default class App extends Component {
             onChangeText={this.placeNameChangedHandler}
             style={styles.placeInput}
           />
-          <Button title='ADD' style={styles.placeButton} />
+          <Button title='ADD' style={styles.placeButton} onPress={this.placeSubmitHandler} />
         </View>
+        <View>{placesOutput}</View>
       </View>
     );
   }
